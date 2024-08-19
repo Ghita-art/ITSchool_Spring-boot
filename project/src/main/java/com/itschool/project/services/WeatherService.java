3e076759927a73ae5221b09de2a2a4770182ad93
Ghita <iceman57187@gmail.com>
2024-08-19 21:18:41 +0300

    -UserController -UserService

---
+++ project/src/main/java/com/itschool/project/services/WeatherService.java
@@ -0,0 +1,11 @@
+package com.itschool.project.services;
+
+import com.itschool.project.models.Weather;
+
+import java.io.IOException;
+
+public interface WeatherService {
+
+    Weather getCityWeather(String city) throws IOException;
+
+}
